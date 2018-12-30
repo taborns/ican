005d7079075253901449b7063fac0c511b0f1277
Tabor Nekatibeb Shiferaw <shiferawtabor@gmail.com>
2018-12-31 01:30:07 +0300

    Blue/purple color shade, about and contact page

---
+++ js/scroll_handler.js
@@ -0,0 +1,36 @@
+$(document).ready(function(e) {
+    backgroundChanged = false;
+    
+    function changeBackUn() {
+        backgroundChanged =!backgroundChanged;
+        $('.main-nav-outer').css('background-color', 'transparent');
+        $('#resources .resource-dropdown').css('background-color','transparent');
+        $('.main-nav li a').css('color','white');
+        
+    }
+
+    function changeBackPre() {
+        backgroundChanged =!backgroundChanged;
+        $('.main-nav-outer').css('background-color', 'white');
+        $('#resources .resource-dropdown').css('background-color', 'white');
+        $('.main-nav li a').css('color','#222');
+        
+    }
+
+    $('#test').scrollToFixed({
+        preUnfixed: function() { changeBackUn() },
+        preFixed: function() { changeBackPre() }
+    });
+
+    $('.res-nav_click').click(function() {
+        $('.main-nav').slideToggle();
+        return false
+
+    });
+
+$('.Portfolio-box').magnificPopup({
+delegate: 'a',
+type: 'image'
+});
+
+});
